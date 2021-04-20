2154606bf7ff236f25cc638fd480bd75de5eae72
NIGERIA\jayeni <jayeni>
2021-04-20 15:46:53 +0100

    Added test for company post login

--- tests/Feature/CompanyAuthenticationTest.php
+++ tests/Feature/CompanyAuthenticationTest.php
@@ -6,6 +6,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\Company;
+use App\Providers\RouteServiceProvider;
 
 class AuthenticateCompany extends TestCase
 {
@@ -14,5 +15,18 @@ class AuthenticateCompany extends TestCase
         $response = $this->get('/company-login');
 
         $response->assertStatus(200);
+    }
+
+    public function test_company_can_authenticate_using_the_login_screen()
+    {
+        $user = Company::factory()->create();
+
+        $response = $this->post('/company-login', [
+            'email' => $user->email,
+            'password' => 'password',
+        ]);
+
+        $this->assertAuthenticated();
+        $response->assertRedirect(RouteServiceProvider::COMPANY_DASHBOARD);
     }
 }
